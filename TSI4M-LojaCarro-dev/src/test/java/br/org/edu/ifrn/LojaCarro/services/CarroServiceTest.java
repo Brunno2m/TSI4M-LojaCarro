d1999be77cf15c8056ca44faccd8c0a03eb8ede2
Brunno2M <brunno.marques2@gmail.com>
2026-05-12 00:00:14 -0300

    "Correção do teste para aprovacação no pipeline CI"

--- TSI4M-LojaCarro-dev/src/test/java/br/org/edu/ifrn/LojaCarro/services/CarroServiceTest.java
+++ TSI4M-LojaCarro-dev/src/test/java/br/org/edu/ifrn/LojaCarro/services/CarroServiceTest.java
@@ -84,7 +84,7 @@ class CarroServiceTest {
         List<Carro> resultado = carroService.findAll();
 
         // Verifica se a lista retornada tem o tamanho esperado (2 carros)
-        assertEquals(80, resultado.size());
+        assertEquals(2, resultado.size());
         verify(carroRepository, times(1)).findAll();
     }
 
